71934981f93829cbf52a973a3a6d527ad5a4c23e
Josué Millán Zamora <jmillan.dev@protonmail.com>
2023-12-06 22:23:29 -0700

    Day 1 puzzle b

--- src/day_01.c
+++ src/day_01.c
@@ -2,26 +2,66 @@
 #include <stdlib.h>
 
 
-int string_to_integer(char * string)
+int string_starts_with(char * string, char * expected)
 {
-    int first_digit = -1,
+    for (int index = 0; expected[index] != '\0'; index++)
+    {
+        if (string[index] == '\0')
+            return 0;
+
+        if (expected[index] != string[index])
+            return 0;
+    }
+
+    return 1;
+}
+
+
+char string_to_integer(char * string)
+{
+    char first_digit = -1,
         last_digit = -1;
 
-    for (int index = 0; string[index] != '\0'; index++)
+    char new_digit = -1;
+
+    for (ssize_t index = 0; string[index] != '\0'; index++)
     {
         if (string[index] >= '0' && string[index] <= '9')
         {
+            new_digit = string[index] - '0';
+        }
+        else
+        {
+            if (string_starts_with(string + index, "zero")) { new_digit = 0; }
+            else if (string_starts_with(string + index, "one")) { new_digit = 1; }
+            else if (string_starts_with(string + index, "two")) { new_digit = 2; }
+            else if (string_starts_with(string + index, "three")) { new_digit = 3; }
+            else if (string_starts_with(string + index, "four")) { new_digit = 4; }
+            else if (string_starts_with(string + index, "five")) { new_digit = 5; }
+            else if (string_starts_with(string + index, "six")) { new_digit = 6; }
+            else if (string_starts_with(string + index, "seven")) { new_digit = 7; }
+            else if (string_starts_with(string + index, "eight")) { new_digit = 8; }
+            else if (string_starts_with(string + index, "nine")) { new_digit = 9; }
+        }
+
+        if (new_digit > 0)
+        {
             if (first_digit == -1)
             {
-                first_digit = string[index] - '0';
+                first_digit = new_digit;
                 last_digit = first_digit;
             }
             else
             {
-                last_digit = string[index] - '0';
+                last_digit = new_digit;
             }
+
+            new_digit = -1;
         }
     }
+
+    if (first_digit == -1)
+        return 0;
 
     return first_digit * 10 + last_digit;
 }
